d7138ceba3090d9de48917ccc6e18be4d8f1f556
Marek Veselý <veselym@spstrutnov.cz>
2022-06-23 16:17:55 +0200

    Inital Commit

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app dark>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-btn color="primary" elevation="1"> Login </v-btn>
+      <v-btn color="primary" elevation="1" outlined> Register </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      fixed: false,
+      title: 'Animu',
+    }
+  },
+}
+</script>
